80230cb26583e960db3b9ac8f2d60b1fd4b55b54
Augusto César <augustocesar4001@gmail.com>
2023-10-19 09:49:40 -0300

    feat: environment configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,29 @@
 import '@/styles/global.css'
 import { type Metadata } from 'next'
+import { Inter, IBM_Plex_Mono } from 'next/font/google'
 import { type ReactNode } from 'react'
+
+import { cn } from '@/lib/utils'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
   description: 'This site was created using the NextJS framework 🚀',
 }
+
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+  display: 'swap',
+  style: ['normal'],
+})
+
+const plexMono = IBM_Plex_Mono({
+  variable: '--font-plex-mono',
+  subsets: ['latin'],
+  weight: ['400', '500'],
+  display: 'swap',
+  style: ['normal'],
+})
 
 interface RootLayoutProps {
   children: ReactNode
@@ -13,7 +31,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="pt-BR" className={cn('antialiased', inter.variable, plexMono.variable)}>
       <body>{children}</body>
     </html>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    container: {
+      center: true,
+      screens: {
+        xl: '1200px',
+      },
+    },
+    extend: {
+      backgroundImage: {
+        'hero-image': "url('/images/hero-bg.png')",
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)', 'sans-serif'],
+        mono: ['var(--font-plex-mono)', 'monospace'],
+      },
+      boxShadow: {
+        button: '0px 0px 68px 7px rgba(5, 150, 105, 0.4)',
+      },
+    },
+  },
+  plugins: [],
+}
